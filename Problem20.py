d2b85047c62d6e878ffd36c885b69f306623402f
msheridan <mspencersheridan@gmail.com>
2018-02-19 19:41:08 -0700

    Finished problem 20

--- Problem20.py
+++ Problem20.py
@@ -6,3 +6,19 @@ and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
 Find the sum of the digits in the number 100!
 """
+import math
+import time
+
+start = time.time()
+
+print(math.factorial(100))
+
+num = "93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000"
+
+sum = 0
+
+for i in range(len(num)):
+    sum += int(num[i])
+
+elapsed = (time.time() - start)
+print("Found %d in %2f seconds" % (sum, elapsed))
