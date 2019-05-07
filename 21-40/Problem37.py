99179dc1c8971c659d81a84c099c8b8ab998cd39
Madison <madisonsadison@gmail.com>
2019-05-06 19:06:56 -0500

    Finished problem 37.

--- 21-40/Problem37.py
+++ 21-40/Problem37.py
@@ -8,25 +8,47 @@ Find the sum of the only eleven primes that are both truncatable from left to ri
 NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
 """
 import time
+import numpy as np
 from Misc.useful import is_prime
+
+
+def test_left_to_right(num):
+    if num == 0:
+        return True
+
+    if is_prime(num):
+        return test_left_to_right(num % (10 ** (len(str(num)) - 1)))
+
+    return False
+
+
+def test_right_to_left(num):
+    if num == 0:
+        return True
+    elif is_prime(num):
+        return test_right_to_left(num // 10)
+
+    return False
 
 
 # driver function for our program
 def main():
     """
     Driver function for our program.  This program is designed to sum the numbers
-    between 1 and 1,000,000 that have the property that both the number and the
-    binary representation of said number are palindroms.
+    between 1 and 1,000,000 that have the given property as you truncate from left
+    to right, and right the left, the number remains prime.
     """
     start_time = time.time()
-    total_numbers = 0
+    numbers = np.array([])
 
-    # loop from 1 to 1,000,000 because 1 is not prime!
+    # loop from 11 to 1,000,000
     for num in range(11, 1000001):
-        break
+        if test_left_to_right(num) and test_right_to_left(num):
+            numbers = np.append(numbers, num)
 
     end_time = time.time() - start_time
-    print("Found %s in %2f seconds." % (sum, end_time))
+    print("Found %s in %2f seconds." % (np.sum(numbers), end_time))
+    print(numbers)
 
 
 # While not required, it is considered good practice to have
