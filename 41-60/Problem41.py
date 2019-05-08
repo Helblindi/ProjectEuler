8f123cabc635e0b0b7134a017795d7a93de2c7a2
Madison <madisonsadison@gmail.com>
2019-05-08 09:28:54 -0500

    Finished problem 31, added a permutation function to useful.py.

--- 41-60/Problem41.py
+++ 41-60/Problem41.py
@@ -6,23 +6,46 @@ is a 4-digit pandigital and is also prime.
 What is the largest n-digit pandigital prime that exists?
 """
 import time
-from Misc.useful import is_pandigital
 from Misc.useful import is_prime
-from Misc.useful import sieve
+from Misc.useful import permutation
 
 
 # driver function for our program
 def main():
     """
+    Generates all possible pandigital numbers and test to see first
+    if they are prime.  If they are prime then it checks if it is
+    bigger than the max num.
     """
     start_time = time.time()
-    num = 0
-    # for i in range(1234567, 987654322):
-    #     if is_prime(i) and is_pandigital(i):
-    #         num = i
+    max_num = 0
 
+    # Generate all possible 7th pandigital numbers to test
+    data1 = list('1234567')
+    for p in permutation(data1):
+        # p is in form ['1', '2', '3', '4', '5', '6', '7'], must be concatenated
+        num = int(''.join(p))
+        # check value
+        if is_prime(num) and num > max_num:
+            max_num = num
+
+    # Generate all possible 8th pandigital numbers to test
+    data2 = list('12345678')
+    for p in permutation(data2):
+        num = int(''.join(p))
+        if is_prime(num) and num > max_num:
+            max_num = num
+
+    # Generate all possible 9th pandigital numbers to test
+    data3 = list('123456789')
+    for p in permutation(data3):
+        num = int(''.join(p))
+        if is_prime(num) and num > max_num:
+            max_num = num
+
+    # calculate end time and print results
     end_time = time.time() - start_time
-    print("Found %s in %2f seconds." % (num, end_time))
+    print("Found %s in %2f seconds." % (max_num, end_time))
 
 
 # While not required, it is considered good practice to have
--- Misc/useful.py
+++ Misc/useful.py
@@ -51,10 +51,32 @@ def is_pandigital(num):
         return False
 
 
-# def generate_pandigital_nums(n):
-#     """
-#     Generates the set of possible nth pandigital numbers.
-#     :param n: nth pandigital
-#     :return: set of possible nth pandigital numbers
-#     """
-#
+# Python function to print permutations of a given list
+def permutation(lst):
+    # If lst is empty then there are no permutations
+    if len(lst) == 0:
+        return []
+
+    # If there is only one element in lst then, only
+    # one permutation is possible
+    if len(lst) == 1:
+        return [lst]
+
+    # Find the permutations for lst if there are
+    # more than 1 characters
+
+    l = []  # empty list that will store current permutation
+
+    # Iterate the input(lst) and calculate the permutation
+    for i in range(len(lst)):
+        m = lst[i]
+
+        # Extract lst[i] or m from the list.  rem_lst is
+        # remaining list
+        rem_lst = lst[:i] + lst[i + 1:]
+
+        # Generating all permutations where m is first
+        # element
+        for p in permutation(rem_lst):
+            l.append([m] + p)
+    return l
